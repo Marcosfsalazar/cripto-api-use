bf1387033671a290bfa2291a39fcf66228277071
Marcos Salazar <Marcos1010521@gmail.com>
2020-12-29 11:10:53 -0400

    finished

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //My api Key
     let apiKey = {
-        key : config.API_KEY
+        key : config.API_KEY  //coinmarketcap key go here
     }
 
     //GET fetch requisition
@@ -10,7 +10,25 @@
         return response.json()
     })
     .then((api)=>{
-        console.log(api)
+        let text = '';
+        //get 10 coins and symbols
+        for(let i = 0; i<10;i++){
+
+            //show api information
+            text = text + `
+            
+            <div class="media">
+                <img src="coin.png" class="align-self-center mr-3" alt="coin image" width="100" height="60">
+                <div class="media-body">
+                <h5 class="mt-2">${api.data[i].name}</h5>
+                <p>${api.data[i].symbol}</p>
+                <p class="blockquote-footer">${api.data[i].first_historical_data}</p>
+                </div>
+            </div>
+            `;
+
+            document.getElementById("coins").innerHTML = text;
+        }
     })
     .catch((error)=>{
         console.error(error.message)
