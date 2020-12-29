dcbcd0099610e80f24c4e87a9acd675bb9cad567
Marcos Salazar <Marcos1010521@gmail.com>
2020-12-29 10:16:20 -0400

    fetch get

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 //My api Key
     let apiKey = {
-        key = config.API_KEY
+        key : config.API_KEY
     }
+
+    //GET fetch requisition
+    fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='+
+    apiKey.key).then((response)=>{
+        if(!response.ok) throw new Error ('Erro ao executar requisição, status '+response.status)
+        return response.json()
+    })
+    .then((api)=>{
+        console.log(api)
+    })
+    .catch((error)=>{
+        console.error(error.message)
+    });
